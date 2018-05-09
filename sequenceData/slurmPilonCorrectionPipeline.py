b753ba6519f20302c084bf863d4224bc6a026f49
Derek Bickhart <dbickhart@mlit.wisc.edu>
2018-05-09 17:14:12 -0500

    fixed pilon jar output

--- sequenceData/slurmPilonCorrectionPipeline.py
+++ sequenceData/slurmPilonCorrectionPipeline.py
@@ -57,7 +57,7 @@ def main(args):
                         curDir + "/" + args.output + "/errLog",
                         False,
                         modules,
-                        1, 2, 8000, -1, args.partition)
+                        1, 3, 8000, -1, args.partition)
     
     # Main loop. Separate chr and values and queue up Pilon jobs
     for k, l in chrLens.items():
@@ -65,7 +65,7 @@ def main(args):
         mem = 8000 if mem < 8000 else mem
         worker.mem(mem)
         
-        cmd = 'java -Xmx{}M -jar $PILON_HOME --genome {} --frags {} --output {}.pilon --outdir {} --fix bases --targets {} --verbose --nostrays'.format(mem, args.genome, args.frag, k, args.output, k)
+        cmd = 'java -Xmx{}M -jar $PILON_HOME/pilon-1.22.jar --genome {} --frags {} --output {}.pilon --outdir {} --fix bases --targets {} --verbose --nostrays'.format(mem, args.genome, args.frag, k, args.output, k)
         worker.createGenericCmd(cmd, "pilon" + k)
         
     ids = worker.queueJobs()
