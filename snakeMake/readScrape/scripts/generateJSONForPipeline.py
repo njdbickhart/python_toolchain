1a59093e5c0ccdf6eaabd3a6d9a840421552d875
Derek Bickhart <dbickhart@mlit.wisc.edu>
2019-05-02 14:47:36 -0500

    replaced masurca with spades

--- snakeMake/readScrape/scripts/generateJSONForPipeline.py
+++ snakeMake/readScrape/scripts/generateJSONForPipeline.py
@@ -15,7 +15,7 @@ if len(sys.argv) == 1:
 print(f'{{\n \"repeatmasker\": \"/path/to/\",\n \"mashexe\": \"/path/to/\",\n \"mashsketch\": \"/path/to/\",\n \"samples\":{{')
 for i in range(1, len(sys.argv)):
     dir=sys.argv[i]
-    for filename in glob.iglob(dir + "/**/*.bam", recursive=True):
+    for filename in glob.iglob(dir + "/**/*realn.bam", recursive=True):
         bname = basename(filename)
         bsegs = re.split(fsep, bname)
         print(f'  \"{bsegs[0]}\" : \"{filename}\",')
