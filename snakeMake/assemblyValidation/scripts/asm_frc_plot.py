fb310fd270c2ce335af145b38b5d1169e0a1e545
Derek Bickhart <njdbickhart@gmail.com>
2020-08-14 14:30:12 -0500

    Start new comparative plotting

---
+++ snakeMake/assemblyValidation/scripts/asm_frc_plot.py
@@ -0,0 +1,8 @@
+import matplotlib
+from matplotlib import pyplot as plt
+matplotlib.use('Agg')
+from matplotlib.collections import BrokenBarHCollection
+from itertools import cycle
+from collections import defaultdict
+import pandas
+import numpy as np
