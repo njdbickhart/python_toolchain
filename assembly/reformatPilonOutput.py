d90e2354ed57541d3815322440d4b741d2478927
Derek Bickhart <njdbickhart@gmail.com>
2020-08-31 13:35:42 -0500

    addition of new argument

--- assembly/reformatPilonOutput.py
+++ assembly/reformatPilonOutput.py
@@ -11,6 +11,7 @@ from os import path
 import io
 import subprocess as sp
 import glob
+import sys
 import concurrent.futures
 
 isChr = re.compile(r'[Cc]hr(.{1,2})_?')
@@ -21,9 +22,13 @@ def arg_parse():
     parser = argparse.ArgumentParser(
             description = "Resolve pilon fastas into karyotypic order"
             )
-    parser.add_argument('-f', '--folder', 
+    parser.add_argument('-f', '--folder',
                         help="The base folder where the pilon fastas are contained",
-                        required=True, type=str
+                        required=False, type=str, default=None
+                        )
+    parser.add_argument('-b', '--file',
+                        help="OR the fasta file to edit",
+                        required=False, type=str, default=None
                         )
     parser.add_argument('-o', '--output',
                         help="Output file full name",
@@ -34,19 +39,26 @@ def arg_parse():
                         default=1, type=int,
                         )
     return parser.parse_args(), parser
-    
+
 def main(args, parser):
+    if args.folder == None and args.file == None:
+        parser.print_help()
+        sys.exit()
+
     # Get a list of fasta files in the directory
     files = []
-    for exts in ('*.fa', '*.fasta'):
-        files.extend(glob.glob('{}/{}'.format(args.folder, exts)))
-        
+    if args.file == None:
+        for exts in ('*.fa', '*.fasta'):
+            files.extend(glob.glob('{}/{}'.format(args.folder, exts)))
+    else:
+        files.append(args.file)
+
     # Now samtools faidx them all and tabulate all chr names
     results = {}
     with concurrent.futures.ProcessPoolExecutor(max_workers=args.threads) as executor:
         for f in files:
             results[f] = executor.submit(samtoolsFaiTask, f)
-            
+
     # Now organize results for sequential samtools faidx processing
     completed = {}
     chrlist = []
@@ -54,7 +66,7 @@ def main(args, parser):
         for c in v.result():
             chrlist.append(c)
             completed[c] = k
-    
+
     # Sort the list by karyotype and then process
     chrlist.sort(key=lambda x: karyotypeSort(x))
     with open(args.output, 'w') as out:
@@ -67,8 +79,8 @@ def main(args, parser):
                 out.write(f'{h}\n')
                 for l in sf.stdout:
                     out.write(l)
-                    
-        
+
+
 def samtoolsFaiTask(file):
     sp.run(f'samtools faidx {file}', shell=True)
     chrs = []
@@ -78,7 +90,7 @@ def samtoolsFaiTask(file):
                 s = l.rstrip().split()
                 chrs.append(s[0])
     return chrs
-  
+
 def karyotypeSort(key):
     c = re.match(isChr, key)
     d = re.match(isNum, key)
@@ -96,9 +108,9 @@ def karyotypeSort(key):
         v = d.group(1)
         return int(v)
     return 103
-            
-        
-          
+
+
+
 if __name__ == "__main__":
     args, parser = arg_parse()
     main(args, parser)
