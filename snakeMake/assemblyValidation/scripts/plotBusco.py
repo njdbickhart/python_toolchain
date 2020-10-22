97cf44f211ad4fb2d81b3bc3189e5831f94faed3
Derek Bickhart <njdbickhart@gmail.com>
2020-10-22 16:25:59 -0500

    busco script made independent of pipeline

--- snakeMake/assemblyValidation/scripts/plotBusco.py
+++ snakeMake/assemblyValidation/scripts/plotBusco.py
@@ -1,3 +1,4 @@
+import argparse
 import matplotlib
 from matplotlib import pyplot as plt
 matplotlib.use('Agg')
@@ -6,36 +7,68 @@ import pandas
 import numpy as np
 import re
 
-asms = snakemake.params["asms"]
-print(asms)
 
-buscos = snakemake.input
-print(list(buscos))
+def parse_user_input():
+    parser = argparse.ArgumentParser(
+            description = "Create a plot of the busco scores"
+            )
+    parser.add_argument('-o', '--output',
+                        help="output file name",
+                        type=str, required=True
+                        )
+    parser.add_argument('-a', '--asms',
+                        help="Expected assembly combinations",
+                        action="append", default=[]
+                        )
+    parser.add_argument('-b', '--buscos',
+                        help="busco file",
+                        action="append", default=[]
+                        )
 
-data = defaultdict(list)
+    return parser.parse_args(), parser
 
-for i, b in enumerate(list(buscos)):
-    with open(b, 'r') as input:
-        for l in b:
-            l = l.strip()
-            if l.startswith('#'):
-                continue
-            elif l.startswith('C'):
-                m = re.match(r'C:.+%\[S:(.+)%,D:(.+)%\],F:(.+)%,M:(.+)%,n:.+', l)
-                data["Assembly"].append(asms[i])
-                data["CompleteSC"].append(float(m.group(1)))
-                data["CompleteDup"].append(float(m.group(2)))
-                data["Fragmented"].append(float(m.group(3)))
-                data["Missing"].append(float(m.group(4)))
-                break
+def main(args, parser):
+    if len(args.asms) < 1 or len(args.buscos) < 1:
+        print("Error! Must input at least one busco file and one assembly label!")
+        parser.print_help()
+        sys.exit()
 
-df = pandas.DataFrame(data)
-#df.set_index("Assembly")
+    #asms = snakemake.params["asms"]
+    #print(asms)
 
-fig, ax = plt.subplots()
+    #buscos = snakemake.input
+    #print(list(buscos))
 
-ax = df[["CompleteSC", "CompleteDup", "Fragmented", "Missing"]].plot.hbar(stacked=True, edgecolor='none')
-ax.legend(bbox_to_anchor=(1.03, 1.0))
+    data = defaultdict(list)
 
-plt.xticks(np.arange(len(asms)), df["Assembly"])
-plt.savefig(snakemake.output)
+    for i, b in enumerate(args.buscos):
+        with open(b, 'r') as input:
+            for l in b:
+                l = l.strip()
+                if l.startswith('#'):
+                    continue
+                elif l.startswith('C'):
+                    m = re.match(r'C:.+%\[S:(.+)%,D:(.+)%\],F:(.+)%,M:(.+)%,n:.+', l)
+                    print(f'{args.asms[i]}\t{m.group(0)}')
+                    data["Assembly"].append(args.asms[i])
+                    data["CompleteSC"].append(float(m.group(1)))
+                    data["CompleteDup"].append(float(m.group(2)))
+                    data["Fragmented"].append(float(m.group(3)))
+                    data["Missing"].append(float(m.group(4)))
+                    break
+
+    df = pandas.DataFrame(data)
+    #df.set_index("Assembly")
+    print(df.describe())
+
+    fig, ax = plt.subplots()
+
+    ax = df[["CompleteSC", "CompleteDup", "Fragmented", "Missing"]].plot.hbar(stacked=True, edgecolor='none')
+    ax.legend(bbox_to_anchor=(1.03, 1.0))
+
+    plt.xticks(np.arange(len(args.asms)), df["Assembly"])
+    plt.savefig(args.output)
+
+if __name__ == "__main__":
+    args, parser = parse_user_input()
+    main(args, parser)
