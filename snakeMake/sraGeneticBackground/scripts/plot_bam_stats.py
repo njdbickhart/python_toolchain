52c2655210659111e6518833770c60ada1fea11f
Derek Bickhart <njdbickhart@gmail.com>
2022-12-20 08:39:58 -0600

    bugfixes and plot improvement

--- snakeMake/sraGeneticBackground/scripts/plot_bam_stats.py
+++ snakeMake/sraGeneticBackground/scripts/plot_bam_stats.py
@@ -10,7 +10,7 @@ import numpy as np
 
 usage = f'python3 {sys.argv[0]} <input tab summary> <output pdf filename>'
 
-if len(sys.argv) != 2:
+if len(sys.argv) != 3:
     print(usage)
     sys.exit(-1)
 
@@ -18,14 +18,18 @@ cols = ["SNAME", "ThreshBp", "Zbp", "Mean", "Q25", "Median", "Q75", "Stdev"]
 
 data = defaultdict(list)
 with open(sys.argv[1], 'r') as input:
-    head = l.readline().rstrip().split()
+    head = input.readline().rstrip().split()
     for l in input:
         s = l.rstrip().split()
         for i, k in enumerate(s):
             data[head[i]].append(k)
 
 df = pd.DataFrame(data)
+df["Zbp"] = df["Zbp"].astype('int32')
+for i in ("Q25", "Median", "Q75", "Stdev"):
+    df[i] = df[i].astype('float')
 
+print(df)
 
 (fig, axis) = plt.subplots(ncols=1, nrows=4)
 axs = axis.flatten().tolist()
@@ -35,19 +39,22 @@ ax = axs.pop()
 ax.set_title("Count of bases under coverage threshold", loc="left")
 ax.set_xlabel("Samples")
 ax.set_ylabel("Bases")
-df[["SNAME", "Zbp"]].plot(x="SNAME", y="Zbp", ax=ax)
+df[["SNAME", "Zbp"]].plot(kind='bar', x="SNAME", y="Zbp", ax=ax)
+ax.set_xticks(ax.get_xticks(), ax.get_xticklabels(), rotation=45, ha='right')
 
 ax = axs.pop()
 ax.set_title("Interquartile coverage values", loc="left")
 ax.set_xlabel("Samples")
 ax.set_ylabel("Coverage")
 df[["SNAME", "Q25", "Median", "Q75"]].plot(x= "SNAME", ax=ax)
+ax.set_xticks(ax.get_xticks(), ax.get_xticklabels(), rotation=45, ha='right')
 
 ax = axs.pop()
 ax.set_title("Coverage Standard Deviation", loc="left")
 ax.set_xlabel("Samples")
 ax.set_ylabel("Stdev")
-df[["SNAME", "Stdev"]].plot(x="SNAME", y="Stdev", ax=ax)
+df[["SNAME", "Stdev"]].plot(kind='bar', x="SNAME", y="Stdev", ax=ax)
+ax.set_xticks(ax.get_xticks(), ax.get_xticklabels(), rotation=45, ha='right')
 
 plt.subplots_adjust(left=0.2, wspace=1.0, top=2.8)
 fig.set_size_inches(21,12)
