9714f60e860d9e9fd70d18a3d00defd0e6bb1d84
Derek Bickhart <njdbickhart@gmail.com>
2024-08-22 11:50:57 +0200

    More output columns

--- snakeMake/sraGeneticBackground/scripts/vcfSVSNPINDELConcordance.py
+++ snakeMake/sraGeneticBackground/scripts/vcfSVSNPINDELConcordance.py
@@ -122,14 +122,15 @@ def calc_concordance(sample_table: list[tuple[int, int]], line: str) -> float:
     if num_calls == 0:
         return -1
     else:
-        return num_agreements / num_calls
+        return (fields[0], fields[1], fields[4], (num_agreements / num_calls))
 
 
 def main():
     """main method of program"""
     args = parse_args()
+    print("Chr\tPos\tSVType\tConcordance")
     for concordance in parse_vcf(args.vcf):
-        print(concordance)
+        print("\t".join(concordance))
 
 
 if __name__ == "__main__":
