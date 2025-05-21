6f11930b9af019f54e81d285c6c1f44d5213e5a4
Derek Bickhart <njdbickhart@gmail.com>
2025-05-21 10:12:36 +0200

    Removed stderr from screening

--- snakeMake/captureSeqAnalysis/scripts/contaminantID.py
+++ snakeMake/captureSeqAnalysis/scripts/contaminantID.py
@@ -15,20 +15,20 @@ with open(sys.argv[2], 'r') as input:
         names.add(l)
 
 with open(sys.argv[1], 'r') as input, open(sys.argv[3], 'w') as filtered, open(sys.argv[4], 'w') as binary:
-    start = False
+    #start = False
     for l in input:
-        if l.startswith('Writing output'):
-            start = True
-            continue
+        #if l.startswith('Writing output'):
+        #    start = True
+        #    continue
 
-        if start:
-            s = l.rstrip().split()
-            if s[4] in names:
-                filtered.write(l)
+        #if start:
+        s = l.rstrip().split()
+        if s[4] in names:
+            filtered.write(l)
 
-                kvalues = s[1].split('/')
-                kratio = int(kvalues[0]) / int(kvalues[1])
+            kvalues = s[1].split('/')
+            kratio = int(kvalues[0]) / int(kvalues[1])
 
-                # Create a binary state if the identification is very strong
-                if kratio > 0.8 or int(s[2]) > 20:
-                    binary.write(f'{s[4]}\n')
+            # Create a binary state if the identification is very strong
+            if kratio > 0.8 or int(s[2]) > 20:
+                binary.write(f'{s[4]}\n')
