86326575eedaf0ccaf322741e9722f83fd617382
Derek Bickhart <njdbickhart@gmail.com>
2025-08-20 11:54:02 +0200

    Modified bamstats plots for multipage PDF

--- snakeMake/captureSeqAnalysis/scripts/plot_bam_stats.py
+++ snakeMake/captureSeqAnalysis/scripts/plot_bam_stats.py
@@ -4,14 +4,15 @@ from collections import defaultdict
 import matplotlib
 matplotlib.use('Agg')
 import matplotlib.pyplot as plt
+from matplotlib.backends.backend_pdf import PdfPages
 from matplotlib.ticker import FuncFormatter
 import seaborn as sns
 import pandas as pd
 import numpy as np
 
-usage = f'python3 {sys.argv[0]} <input tab summary> <output pdf filename>'
+usage = f'python3 {sys.argv[0]} <input tab summary> <samples per page> <output pdf filename>'
 
-if len(sys.argv) != 3:
+if len(sys.argv) != 4:
     print(usage)
     sys.exit(-1)
 
@@ -29,7 +30,7 @@ def space_labels(ax, nsample):
         if i % displayidx != 0:
             l.set_visible(False)
 
-def cov_thresh(ax):
+def cov_thresh(ax, df):
     ax.set_title("Count of bases under coverage threshold", loc="left")
 
     ax.set_ylabel("Bases")
@@ -40,7 +41,7 @@ def cov_thresh(ax):
     sns.barplot(data=df, x='SNAME', y='Zbp', ax=ax)
     ax.set_xlabel("")
 
-def iq_values(ax):
+def iq_values(ax, df):
     ax.set_title("Interquartile coverage values", loc="left")
 
     ax.set_ylabel("Coverage")
@@ -53,7 +54,7 @@ def iq_values(ax):
     ax.bxp(boxes, positions=[x for x in range(len(ax.get_xticks()))], showfliers=False)
     ax.set_xlabel("")
 
-def cov_plot(ax):
+def cov_plot(ax, df):
     ax.set_title("Coverage Mean", loc="left")
 
     ax.set_ylabel("Mean X Coverage")
@@ -68,7 +69,7 @@ def cov_plot(ax):
     ax.axhline(avg, linestyle='--')
     ax.annotate(f'Avg X Coverage: {avg:.2f}', (int(len(df.index) / 2), avg), xytext=(0.35, 0.55), textcoords='axes fraction', arrowprops=dict(facecolor='blue', arrowstyle='wedge'), fontsize=14, color='b')
 
-def heatmap_plot(ax, cbar_ax):
+def heatmap_plot(ax, df, cbar_ax):
     ax.set_xlabel("Samples")
     tdf = df[["SNAME", 'Zbp', 'sub15', 'sub30', 'gt30']].set_index('SNAME').copy()
     tdf.index = [''.join(col) for col in tdf.index.values]
@@ -79,10 +80,12 @@ cols = ["SNAME", "ThreshBp", "Zbp", "Mean", "Q25", "Median", "Q75", "Max", "Stde
 'sub15', 'sub30', 'gt30']
 
 data = defaultdict(list)
+linecount = 0
 with open(sys.argv[1], 'r') as input:
     head = input.readline().rstrip().split()
     for l in input:
         s = l.rstrip().split()
+        linecount += 1
         for i, k in enumerate(s):
             data[head[i]].append(k)
 
@@ -95,28 +98,39 @@ for i in ('sub15', 'sub30', 'gt30'):
     df[i] = df[i].astype('float')
 
 print(df)
+splits = int(sys.argv[2])
 
-(fig, axis) = plt.subplots(ncols=2, nrows=4, sharex='col', constrained_layout=True, gridspec_kw={'width_ratios' : [100, 1]})
+figures = list()
+# New feature: split up the list of samples if they exceed a theshold and print their plots on separate PDF pages
+for i in range(0, linecount, splits):
+    end = i + splits
+    if end >= linecount:
+        end = linecount -1
+    tdf = df.iloc[i:end, :].copy()
+    (fig, axis) = plt.subplots(ncols=2, nrows=4, sharex='col', constrained_layout=True, gridspec_kw={'width_ratios' : [100, 1]})
 
-axis[0,1].remove()
-axis[1,1].remove()
-axis[2,1].remove()
+    axis[0,1].remove()
+    axis[1,1].remove()
+    axis[2,1].remove()
 
-ax = axis[0,0]
-cov_thresh(ax)
+    ax = axis[0,0]
+    cov_thresh(ax, tdf)
 
-ax = axis[1,0]
-iq_values(ax)
+    ax = axis[1,0]
+    iq_values(ax, tdf)
 
-ax = axis[2,0]
-cov_plot(ax)
+    ax = axis[2,0]
+    cov_plot(ax, tdf)
 
-ax = axis[3,0]
-heatmap_plot(ax, axis[3,1])
+    ax = axis[3,0]
+    heatmap_plot(ax, tdf, axis[3,1])
 
-fig.set_size_inches(21,12)
-#fig.tight_layout()
+    fig.set_size_inches(21,12)
+    fig.tight_layout()
+    figures.append(fig)
 
-plt.savefig(sys.argv[2])
+with PdfPages(sys.argv[3]) as pdf:
+    for f in figures:
+        pdf.savefig(sys.argv[3], bbox_inches='tight')
 
 print('Fini')
